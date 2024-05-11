43789c87eaadff3507c151e23a3300629d649172
Miyashooooo <122672964+cstrikeasia@users.noreply.github.com>
2024-05-12 04:22:14 +0800

    Add files via upload

--- js/fetch.js
+++ js/fetch.js
@@ -3,7 +3,7 @@ let ws;
 
 function connectWebSocket() {
     ws = new WebSocket(wsURL);
-	const token = 'xxx';
+	const token = 'bWl5YXNob29vb29fdGVzdA';
     ws.addEventListener("open", function (event) {
         console.log("WebSocket connected");
         sendWebSocketMessage({ type: 'region', token: token });
@@ -66,6 +66,7 @@ function sendWebSocketMessage(t) {
 
 let Report_Info_Msg = '';
 function reportFormat(data){
+	Report_Info_Msg = '';
     const loc = data.loc;
     const dep = data.depth;
     const lat = data.lat;
